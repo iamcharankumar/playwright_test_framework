9aa9a2568de6f85b7a80cafbfc4f86c7668d68fd
Charankumar H <charankumarh@Charankumars-MacBook-Pro.local>
2025-03-11 22:38:39 +0530

    Resotred headless logic for MSEdge.

--- src/main/java/io/swaglabs/portal/qa/browsermanager/MsEdgeBrowser.java
+++ src/main/java/io/swaglabs/portal/qa/browsermanager/MsEdgeBrowser.java
@@ -7,14 +7,16 @@ import com.microsoft.playwright.Playwright;
 import io.swaglabs.portal.qa.constants.BrowserName;
 import io.swaglabs.portal.qa.constants.WebPortalConstants;
 
-import java.util.Collections;
+import java.util.List;
 
 public class MsEdgeBrowser implements IBrowser {
     @Override
     public BrowserContext createSession(Playwright playwright, boolean isHeadless) {
         return playwright.chromium().launch(new BrowserType.LaunchOptions()
                         .setHeadless(isHeadless)
-                        .setArgs(Collections.singletonList(WebPortalConstants.MAXIMIZE_WINDOW))
+                        .setArgs(isHeadless
+                                ? List.of(WebPortalConstants.NEW_HEADLESS, WebPortalConstants.MAXIMIZE_WINDOW)
+                                : List.of(WebPortalConstants.MAXIMIZE_WINDOW))
                         .setChannel(BrowserName.MS_EDGE.getBrowserType()))
                 .newContext(new Browser.NewContextOptions().setViewportSize(null));
     }
