e8e2646274968b11936529f2c8c3a5b7beb70bf0
Charankumar H <charankumarh@Charankumars-MacBook-Pro.local>
2025-04-14 23:58:20 +0530

    Refactored Browser Factory ENUM.

--- src/main/java/io/swaglabs/portal/qa/browsermanager/BrowserFactory.java
+++ src/main/java/io/swaglabs/portal/qa/browsermanager/BrowserFactory.java
@@ -4,10 +4,10 @@ public class BrowserFactory {
 
     public IBrowser createBrowser(String browserName) {
         return switch (BrowserName.fromString(browserName)) {
-            case CHROME -> new ChromeBrowser();
             case FIREFOX -> new FirefoxBrowser();
             case WEBKIT -> new WebkitBrowser();
             case MS_EDGE -> new MsEdgeBrowser();
+            default -> new ChromeBrowser();
         };
     }
 }
