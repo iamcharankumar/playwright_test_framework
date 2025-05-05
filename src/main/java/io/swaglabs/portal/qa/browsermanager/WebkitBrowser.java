e334ccbf631532034fa3abda7d37fd1cdb429e84
Charankumar H <charankumarh@Charankumars-MacBook-Pro.local>
2025-05-05 23:38:57 +0530

    Removed maximized start.

--- src/main/java/io/swaglabs/portal/qa/browsermanager/WebkitBrowser.java
+++ src/main/java/io/swaglabs/portal/qa/browsermanager/WebkitBrowser.java
@@ -6,14 +6,11 @@ import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
 import io.swaglabs.portal.qa.constants.WebPortalConstants;
 
-import java.util.Collections;
-
 public class WebkitBrowser implements IBrowser {
     @Override
     public BrowserContext createSession(Playwright playwright, boolean isHeadless) {
         return playwright.webkit().launch(new BrowserType.LaunchOptions()
-                        .setHeadless(isHeadless)
-                        .setArgs(Collections.singletonList(WebPortalConstants.MAXIMIZE_WINDOW)))
+                        .setHeadless(isHeadless))
                 .newContext(new Browser.NewContextOptions()
                         .setViewportSize(WebPortalConstants.SCREEN_WIDTH, WebPortalConstants.SCREEN_HEIGHT));
     }
